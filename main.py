a579902b47eb5cb62f76800d8ac7b18b365bb17a
Matthew <mbraun5@csu.fullerton.edu>
2019-06-28 01:14:34 -0700

    edited formatting

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ class Main(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.configure(background='#465362')
+        self.grid_columnconfigure(0, weight=1)
+
         self.navbar = nav.NavBar(self)
         self.navbar.grid(row=0, column=0, sticky='ew')
+
         self.shortcut_frame = sf.ShortcutFrame(self)
         self.shortcut_frame.grid(row=1, column=0, sticky='n')
-        self.grid_columnconfigure(0, weight=1)
 
         self.bind('<Alt_L>f', self.navbar.keyevent)
         self.bind('<Alt_L>e', self.navbar.keyevent)
@@ -22,12 +24,10 @@ class Main(tk.Tk):
         self.bind('<KeyRelease>', self.navbar.altevent)
         self.bind('<ButtonRelease-1>', self.navbar.buttonevent)
 
-        self.lastKey = None
-
 
 if __name__ == "__main__":
     root = Main()
-    root.geometry('500x300+{}+{}'.format(int(pag.size()[0] / 2 - 500/2), int(pag.size()[1] / 2 - 350/2)))
+    root.geometry('600x400+{}+{}'.format(int(pag.size()[0] / 2 - 500/2), int(pag.size()[1] / 2 - 350/2)))
     root.minsize(250, 0)
     root.title('Automation')
     root.mainloop()
--- shortcutframe.py
+++ shortcutframe.py
@@ -7,30 +7,42 @@ class ShortcutFrame(tk.Frame):
         tk.Frame.__init__(self)
         self.master = master
 
-        self.configLabel = tk.Label(self, text="Configure Global Keyboard Shortcuts")
-        self.configLabel.grid(row=0, column=0)
+        self.titleFrame = tk.Frame(self)
+        self.titleFrame.pack()
+        self.bodyFrame = tk.Frame(self)
+        self.bodyFrame.pack()
 
-        self.getPositionLabel = tk.Label(self, text="Get Mouse Cursor Position:")
-        self.getPositionLabel.grid(row=1, column=0)
-        self.startStopLabel = tk.Label(self, text="Start / Stop Script Execution:")
-        self.startStopLabel.grid(row=2, column=0)
+        self.configLabel = tk.Label(self.titleFrame, text="Configure Global Keyboard Shortcuts")
+        self.configLabel.pack()
+        self.getPositionLabel = tk.Label(self.bodyFrame, text="Get Mouse Cursor Position:")
+        self.getPositionLabel.grid(row=1, column=0, padx=10, sticky='e')
+        self.startStopLabel = tk.Label(self.bodyFrame, text="Start / Stop Script Execution:")
+        self.startStopLabel.grid(row=2, column=0, padx=10, sticky='e')
 
-        self.getPositionText = tk.Text(self, state="disabled", width=15, height=1)
-        self.getPositionText.grid(row=1, column=1)
-        self.startStopText = tk.Text(self, state="disabled", width=15, height=1)
-        self.startStopText.grid(row=2, column=1)
+        self.getPositionText = tk.Text(self.bodyFrame, state="disabled", width=15, height=1, borderwidth=2,
+                                       relief='groove')
+        self.getPositionText.grid(row=1, column=1, padx=10, pady=5)
+        self.startStopText = tk.Text(self.bodyFrame, state="disabled", width=15, height=1, borderwidth=2,
+                                     relief='groove')
+        self.startStopText.grid(row=2, column=1, padx=10, pady=5)
+        self.getPositionText.tag_config("justify", justify='center')
+        self.startStopText.tag_config("justify", justify='center')
         keyboard.on_press(self.handle_press)
         self.textDict = {self.getPositionText: 'None',
                          self.startStopText: 'None'}
 
-        self.getPositionAssign = tk.Button(self, text="Assign", command=lambda: print("PositionAssign"))
-        self.getPositionAssign.grid(row=1, column=2)
-        self.startStopAssign = tk.Button(self, text="Assign", command=lambda: print("StartAssign"))
-        self.startStopAssign.grid(row=2, column=2)
-        self.getPositionClear = tk.Button(self, text="Clear", command=lambda: self.clear(self.getPositionText))
-        self.getPositionClear.grid(row=1, column=3)
-        self.startStopClear = tk.Button(self, text="Clear", command=lambda: self.clear(self.startStopText))
-        self.startStopClear.grid(row=2, column=3)
+        self.getPositionAssign = tk.Button(self.bodyFrame, text="Assign", width=10, borderwidth=1,
+                                           command=lambda: print("PositionAssign"))
+        self.getPositionAssign.grid(row=1, column=2, padx=10)
+        self.startStopAssign = tk.Button(self.bodyFrame, text="Assign", width=10, borderwidth=1,
+                                         command=lambda: print("StartStopAssign"))
+        self.startStopAssign.grid(row=2, column=2, padx=10)
+        self.getPositionClear = tk.Button(self.bodyFrame, text="Clear", width=10, borderwidth=1,
+                                          command=lambda: self.clear(self.getPositionText))
+        self.getPositionClear.grid(row=1, column=3, padx=10)
+        self.startStopClear = tk.Button(self.bodyFrame, text="Clear", width=10, borderwidth=1,
+                                        command=lambda: self.clear(self.startStopText))
+        self.startStopClear.grid(row=2, column=3, padx=10)
 
     def handle_press(self, key):
         widget = self.master.focus_displayof()
@@ -39,7 +51,7 @@ class ShortcutFrame(tk.Frame):
                 return
             widget.config(state='normal')
             widget.delete(1.0, 'end')
-            widget.insert('end', key.name)
+            widget.insert('end', key.name, "justify")
             widget.config(state='disabled')
             self.textDict[self.getPositionText] = key.name
         elif widget == self.startStopText:
@@ -47,7 +59,7 @@ class ShortcutFrame(tk.Frame):
                 return
             widget.config(state='normal')
             widget.delete(1.0, 'end')
-            widget.insert('end', key.name)
+            widget.insert('end', key.name, "justify")
             widget.config(state='disabled')
             self.textDict[self.startStopText] = key.name
         self.master.focus()
